b6137a6ce9b2d854151b089011328dcf8a562aeb
PresbyZM <presbyzenilmacotela2@gmail.com>
2023-01-29 13:38:51 -0600

    Practica 5y6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { AboutComponent } from './pages/about/about.component';
-import { HomeComponent } from './pages/home/home.component';
+//import { ContactComponent } from './pages/contact/contact.component';
+//import { AboutComponent } from './pages/about/about.component';
+//import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent,
+    /*ContactComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,*/
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
